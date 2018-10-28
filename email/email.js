0386a6bb40f8e1b8d2d1489872e3e1006ec261b7
PSoysauce <32037100+PSoysauce@users.noreply.github.com>
2018-10-27 22:31:36 -0500

    Uptaded the email and summary function
    helped format the email feature, getSummary was updated to include sentiment values

--- email/email.js
+++ email/email.js
@@ -2,9 +2,10 @@
  * function sends the transcript 
  */
 var nodemailer = require('nodemailer');
-emailTranscript("10/25/18", "Log file");
+var log = "thank you for calling T-Mobile this is Patrick\nhello I am having issues with Cellular Connection here in Austin\nokay let me check the service that is over there\nall right so after checking it appears Austin is all right are you getting any signal on your phone or do\nyou have no bars at all I have none at all\nall right did you check that cellular data is turned on in your settings I have checked and there's on\nall right then you you'll have to bring your phone into a local T-Mobile store to get it fixed\nall right thank you\nyou're welcome have a nice day\n";
+emailTranscript("10/25/18", log, "ningchen227@gmail.com");
 
-function emailTranscript(transcriptDate, log) {
+function emailTranscript(transcriptDate, log, email) {
     var emailer = nodemailer.createTransport({
         service: 'gmail',
         auth: {
@@ -14,9 +15,13 @@ function emailTranscript(transcriptDate, log) {
     });
     var emailOpt = {
         from: 'hacktobercustomerservice@gmail.com',
-        to: 'psoysauce@gmail.com',
+        to: email,
         subject: 'Your call to T-Mobile on ' + transcriptDate,
-        text: log
+        html: "<html>" + 
+        "<img style='width:25%;height:25%;' src='https://images.techhive.com/images/article/2014/08/tmobile_logo-100369081-large.jpg'></img>" + 
+        "<p style='font-family:Verdana, Geneva, Tahoma, sans-serif; font-size:14px;'>Thank you for calling T-Mobile! We appreciate your business. Here is a transcription of your conversation with us at " + transcriptDate + ": </p>" +
+        "<p style='font-family:Verdana, Geneva, Tahoma, sans-serif; font-size:12px;'>" + log + "</p>" +
+        "</html>"
     };
     emailer.sendMail(emailOpt, function(error, info){
         if (error) {
--- getSummary.js
+++ getSummary.js
@@ -1,86 +1,132 @@
-let str = "Hello, I am having issues connecting to the cellular service here. Okay, let me check the service in your area real quick. Just give me a moment okay? " +
-    "Okay" + "Alright, it looks like the cell towers over there are overloaded. We have technicians working on the solution, a fix to the problem is being implemented currently, cellular service should be back to normal in about a few hours." +
-    "A few hours? Okay. Thank you for the information. No problem, you have a good day. I also had trouble 4glte connection.";
+const NLU_MODULE = require('watson-developer-cloud/natural-language-understanding/v1.js');
 
-console.log(getSummary(str));
-function getSummary(str){
-    //console.log(str);
-    
-        let summary = "Issues: \n";
-        //user problem
-        let keyWords = ['trouble', 'issue', 'problem'];
-        let keyWordStartPos = -1;  
-        let j = 0;  
-        for (let i = 0; i < keyWords.length; i++) {//loops through each possible keyword
-            keyWordStartPos = -1;//sets the start pos to the beg each iteration
-            //console.log("Searching for keyword " + keyWords[i]);
-            for(let j = 0; j < str.length; j++) {//length of the string of the conversation
-                // Pick up keyword, get the next sentence or so, etc, save it to some 
-                //  letiable or write it to the summary
-                let foundWordPos = str.indexOf(keyWords[i], keyWordStartPos+1); 
-                //console.log(str.indexOf('.'));
-                if(foundWordPos > -1)
-                {
-                    let periodPos = findPeriod(str, foundWordPos);
-                    let summaryPart = "";
-                    if(periodPos != -1) {
-                        summaryPart = str.substr(foundWordPos, periodPos-foundWordPos) + "\n";
-                    } else {
-                        summaryPart = str.substr(foundWordPos, str.length-foundWordPos) + "\n";
-                    }
-                    summary += summaryPart; 
-                    //console.log("Current keyword: " + keyWords[i] + ", found at position " + foundWordPos);
-                    //console.log("Summary added: " + summaryPart);
-    
-                    j = foundWordPos;
-                    keyWordStartPos = foundWordPos; 
-                }
-                
-            }
-    
-        }
-        summary += "\nSolutions: \n";
-        let solutionW = ['solution', 'answer', 'fix'];
-        keyWordStartPos = -1;  
-        j = 0;  
-        for (let i = 0; i < solutionW.length; i++) {//loops through each possible keyword
-            keyWordStartPos = -1;//sets the start pos to the beg each iteration
-            //console.log("Searching for keyword " + solutionW[i]);
-            for(let j = 0; j < str.length; j++) {//length of the string of the conversation
-                // Pick up keyword, get the next sentence or so, etc, save it to some 
-                //  letiable or write it to the summary
-                let foundWordPos = str.indexOf(solutionW[i], keyWordStartPos+1); 
-                //console.log(str.indexOf('.'));
-                if(foundWordPos > -1)
-                {
-                    let periodPos = findPeriod(str, foundWordPos);
-                    let summaryPart = "";
-                    if(periodPos != -1) {
-                        summaryPart = str.substr(foundWordPos, periodPos-foundWordPos) + "\n";
-                    } else {
-                        summaryPart = str.substr(foundWordPos, str.length-foundWordPos) + "\n";
-                    }
-                    summary += summaryPart; 
-                    //console.log("Current keyword: " + solutionW[i] + ", found at position " + foundWordPos);
-                    //console.log("Summary added: " + summaryPart);
-    
-                    j = foundWordPos;
-                    keyWordStartPos = foundWordPos; 
-                }
-                
-            }
-    
-        }
+let str = "thank you for calling T-Mobile this is Patrick\nhello I am having issues with Cellular Connection here in Austin\nokay let me check the service that is over there\nall right so after checking it appears Austin is all right are you getting any signal on your phone or do\nyou have no bars at all I have none at all\nall right did you check that cellular data is turned on in your settings I have checked and there's on\nall right then you you'll have to bring your phone into a local T-Mobile store to get it fixed\nall right thank you\nyou're welcome have a nice day\n";
 
-        //console.log("Finished parsing");
+const bluemix_auth = require('./auth/bluemix.json')
+
+const nlu = new NLU_MODULE(bluemix_auth);
+
+function getSummary(keywords, keywordsScore, str) {
+    let issueSummary = "Issues: \n";
+    let solSummary = "\nSolutions: \n";
+    for(var i = 0; i < keywords.length; i++){
+        /*if((keywordsScore[i] == 0){
+            
+        }*/
+        if(keywordsScore[i] < 0){ 
+            var splitStr = str.split('\n');
+            splitStr.forEach(element => {
+
+                if(element.includes(keywords[i]) && !issueSummary.includes(keywords[i])) {
+                    issueSummary += element + "\n";                    
+                } 
+            })
+            str.indexOf(keywords[i]);
+        }
+        else if(keywordsScore[i] > 0) {
+            //positive
+            var splitStr = str.split('\n');
+            splitStr.forEach(element => {
+                if(element.includes(keywords[i]) && !solSummary.includes(keywords[i])) {
+                    solSummary += element + "\n";                    
+                } 
+            })
+            str.indexOf(keywords[i]);
+        }
+    }
+
+    /*
+    let jsonObj = JSON.parse(element);
+    jsonObj.relations.forEach(word => {
         
-        //solution
-        //console.log(summary);
-        return summary;
+    })*/
+    return issueSummary + solSummary;
+}
+
+function findNewLine(str, startIndex) {
+    for (let i = startIndex; i < str.length; i++) {
+        if (str.charAt(i) == '\n') return i;
     }
-    function findPeriod(str, startIndex) {
-        for(let i = startIndex; i < str.length; i++) {
-            if(str.charAt(i) == '.') return i;
+    return -1;
+}
+
+/*DO NOT TOUCH*/
+// this function is literally dynamite. no touchy.
+async function processText(inputText) {
+    let analysis_array = []
+
+    let ctr = 0;
+    let sentences = inputText.split("\n")
+    for (let i = 0; i < sentences.length; i++) {
+        let sentence = sentences[i]
+
+        if (sentence.length !== 0) {
+            var parameters = {
+                'text': sentence,
+                'features': {
+                    'relations': {},
+                    'entities': {
+                        'emotion': true,
+                        'sentiment': true,
+                        'limit': 10
+                    },
+                    'keywords': {
+                        'emotion': true,
+                        'sentiment': true,
+                        'limit': 100
+                    }
+                }
+            }
+            nlu.analyze(parameters, async function (err, response) {
+                if (err) {
+                    console.log('error:', err);
+                } else {
+                    var jsonResult = JSON.stringify(response, null, 2);
+
+                    ctr++;
+                    analysis_array.push(jsonResult)
+
+                    if (ctr === sentences.length - 1) {
+                        doAnalysis(analysis_array)
+                    }
+                }
+            });
+
         }
-        return -1;
     }
+}
+
+/**
+ * Array is the processed array of json objects.
+ * Do whatever the fuck you want with it.
+ */
+function doAnalysis(array) {
+    var keywords = [];
+    var keywordsScore = [];
+    var avg;
+    var total = 0;
+    array.forEach(element => {
+        let jsonObj = JSON.parse(element)
+
+       // keyswords is an array
+       // console.log(jsonObj.keywords)
+       let average = 0;
+        jsonObj.keywords.forEach(word => {
+            total += word.sentiment.score;
+            let text = word.text
+            let score = word.sentiment.score
+            let label = word.sentiment.label
+            let relevance = word.sentiment.relevance                                              
+
+            console.log(text, score, label, relevance);
+            keywords.push(text);
+            keywordsScore.push(score);
+        })
+        
+    })
+    average = (total / array.length);
+    console.log("Average value: " + average);
+    console.log(getSummary(keywords, keywordsScore, str));
+}
+
+processText(str)
